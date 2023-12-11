b4f3936c89370321cf4c065921661ec21542c1a0
leiber bertel <leiberbertel777@gmail.com>
2023-12-10 23:05:00 -0500

    feat: add header and footer component

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,57 @@
+<template>
+  <footer class="footer">
+    <div class="content">
+      <div class="copy">
+        <a href="https://leiberbertel.github.io/" target="_blank">
+          © {{ new Date().getFullYear() }} Leiber Bertel
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+  
+<script>
+export default {
+  name: "FooterComponent"
+}
+</script>
+  
+<style>
+.copy {
+  position: absolute;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+a {
+  display: flex;
+  position: relative;
+  text-decoration: none;
+  color: #fff
+}
+
+.copy a::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: -5px;
+  height: 1px;
+  background-color: #fff;
+  transform: scaleX(0);
+  transition: transform 0.3s ease-in-out;
+}
+
+.copy a:hover::after {
+  transform: scaleX(1);
+}
+
+.footer {
+  padding: 40px 0;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
+  
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="header">
+    <router-link to="/" class="logo">
+      Task App
+    </router-link>
+    <nav class="nav">
+      <router-link to="/">Iniciar sesión</router-link>
+      <router-link to="/register">Registrarse</router-link>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 30px;
+    position: relative;
+  }
+  .logo {
+    padding: 10px 30px;
+    font-size: 24px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    color: #fff;
+  }
+  .nav {
+    display: flex;
+    gap: 25px;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .nav a {
+    font-size: 1em;
+    text-decoration: none;
+    color: #fff;
+    position: relative; 
+  }
+  .nav a::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: -5px;
+    height: 1px;
+    background-color: #fff;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+  .nav a:hover::after {
+    transform: scaleX(1); 
+  }
+</style>
