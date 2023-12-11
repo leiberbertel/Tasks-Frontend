29888effd7f5573089d53032152063f7b2a3e1aa
leiber bertel <leiberbertel777@gmail.com>
2023-12-10 23:04:14 -0500

    feat: add custom tags for header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <header-nav></header-nav>
   <div id="app">
     <main>
       <router-view />
     </main>
+    <footer-tag></footer-tag>
   </div>
 </template>
 
@@ -15,5 +17,14 @@ export default {
 <style>
 #app {
   text-align: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('../public/bg-main.jpg');
+  background-size: cover;
+  background-position: center;
+  z-index: -1;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue';
 import router from './router';
 import axios from 'axios';
 
+import Header from './components/HeaderComp';
+import Footer from './components/FooterComp';
+
 axios.defaults.baseURL = 'http://localhost:8080';
 
 const app = createApp(App);
 app.use(router);
+
+app.component('header-nav', Header)
+app.component('footer-tag', Footer)
 
 app.mount('#app');
 
