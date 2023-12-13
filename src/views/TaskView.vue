eeecf69ee302d0744bce63423036ae22df281f37
leiber bertel <leiberbertel777@gmail.com>
2023-12-13 17:56:41 -0500

    style: remove comments

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -2,7 +2,6 @@
   <div class="todo-container">
     <div class="todo-header">
       <h1>To-Dos</h1>
-      <!-- Aquí puedes agregar los botones o filtros como "All Tasks", "Board", etc. -->
     </div>
     <div class="todo-list">
       <div class="todo-item" v-for="task in formattedTasks" :key="task.id">
@@ -35,10 +34,7 @@ export default {
   },
   methods: {
     formatDate(date) {
-      // Verifica si date es una cadena no vacía antes de proceder.
       if (date && typeof date === 'string' && date.trim() !== '') {
-        // Asegúrate de que la fecha tenga el formato esperado.
-        // Java LocalDateTime no tiene zona horaria, así que agregamos 'Z' para UTC.
         if (!date.includes('Z') && !date.includes('+')) {
           date += 'Z';
         }
@@ -48,7 +44,7 @@ export default {
           return parsedDate.toLocaleDateString(undefined, options);
         }
       }
-      return 'Fecha no válida'; 
+      return 'Fecha no válida';
     }
   },
   async created() {
